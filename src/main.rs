92adec90c085f20ed409e566f2a17c9ce3d16c40
FeistyKit <eeveebeevee33@gmail.com>
2022-01-23 21:20:50 -0500

    cleaned it up; changed it to use clap

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,34 @@
 #![allow(clippy::or_fun_call)]
+
+use clap::Parser;
 use phf::{phf_map, Map};
 use std::{
     collections::BTreeMap,
     fs::{self, File},
-    io::Write,
+    io::Write, error::Error,
 };
 
 use image::{open, ImageBuffer, Luma};
+
+#[derive(Parser)]
+#[clap(
+    name = "Ascii Converter",
+    author = "FeistyKit <eeveebeevee33@gmail.com>"
+)]
+struct Args {
+
+    #[clap(short, long, default_value_t = 16)]
+    width_compression: u32,
+
+    #[clap(short, long, default_value_t = 8)]
+    height_compression: u32,
+
+    input_filepath: String,
+
+    #[clap(short, long, default_value = "output.txt")]
+    output_filepath: String
+}
+
 static DARKNESS_MAP: Map<u8, char> = phf_map! {
     0u8 => ' ',
     1u8 => '.',
@@ -19,49 +41,32 @@ static DARKNESS_MAP: Map<u8, char> = phf_map! {
     8u8 => '%',
     9u8 => '@',
 };
-fn main() {
-    let (map, string_prompt) = prepare_map();
-    let initial_image = open_from_map(&map, &string_prompt);
-    let details = prepare_image_details(&initial_image);
+
+type MaybeError = Result<(), Box<dyn Error>>;
+
+fn main() -> MaybeError {
+    let args = Args::parse();
+    let initial_image = open_file(&args.input_filepath)?;
+    let details = prepare_image_details(&initial_image, &args);
     let output_string = pixel_each(initial_image, details);
-    save(output_string);
-    pause();
+    save(output_string, &args.output_filepath)
 }
 
-fn save(output_string: String) {
-    let mut b = File::create("output.txt").unwrap();
-    b.write_all(output_string.as_bytes()).unwrap();
+fn save(output_string: String, path: &str) -> MaybeError {
+    let mut b = File::create(path)?;
+    b.write_all(output_string.as_bytes())?;
     println!("Finished!");
+    Ok(())
 }
 
-fn prepare_image_details(initial_image: &ImageBuffer<Luma<u8>, Vec<u8>>) -> ImageDetails {
+fn prepare_image_details(initial_image: &ImageBuffer<Luma<u8>, Vec<u8>>, args: &Args) -> ImageDetails {
     let (w, h) = initial_image.dimensions();
-    let split_h = input("Image height step (default 16): ")
-        .unwrap_or("16".to_string())
-        .trim()
-        .parse::<u32>()
-        .unwrap_or(16);
-    let split_w = input("Image width step (default 8): ")
-        .unwrap_or("8".to_string())
-        .trim()
-        .parse::<u32>()
-        .unwrap_or(8);
     ImageDetails {
         w,
         h,
-        split_w,
-        split_h,
+        split_w: args.width_compression,
+        split_h: args.height_compression,
     }
-}
-
-fn prepare_map() -> (BTreeMap<usize, String>, String) {
-    let map = file_map();
-    let mut string_prompt =
-        "Input the number that corresponds to the file that you want to convert: ".to_string();
-    for (k, v) in map.iter() {
-        string_prompt.push_str(&format!("\n{}: {}", k, v));
-    }
-    (map, string_prompt)
 }
 
 //goes through pixels to determine the darkness of each
@@ -85,12 +90,6 @@ fn pixel_each(initial_image: ImageBuffer<Luma<u8>, Vec<u8>>, details: ImageDetai
     }
     output_string
 }
-fn input(m: &str) -> Result<String, std::io::Error> {
-    println!("{}", m);
-    let mut s = String::new();
-    std::io::stdin().read_line(&mut s)?;
-    Ok(s)
-}
 //gets char from the map depending on darkness value. the math is so that the value can be changed to 10 options.
 fn get_char(i: u8) -> char {
     //For users who use a light background; will add an option to switch depending on text viewer.
@@ -100,44 +99,6 @@ fn get_char(i: u8) -> char {
 fn open_file(p: &str) -> Result<ImageBuffer<Luma<u8>, Vec<u8>>, Box<dyn std::error::Error>> {
     let b = open(p)?.into_luma8();
     Ok(b)
-}
-//creates map to get user input
-fn file_map() -> BTreeMap<usize, String> {
-    let b = fs::read_dir("./").expect("Could not read directory!");
-    let mut x = 1;
-    let mut map = BTreeMap::new();
-    for i in b {
-        let h = i.unwrap().file_name();
-        let q = h.to_str().unwrap();
-        if q.contains(".png") || q.contains(".jpg") {
-            map.insert(x, q.to_string());
-            x += 1;
-        }
-    }
-    map
-}
-fn open_from_map(
-    map: &BTreeMap<usize, String>,
-    string_prompt: &str,
-) -> ImageBuffer<Luma<u8>, Vec<u8>> {
-    match map.get(
-        &input(string_prompt)
-            .unwrap()
-            .trim()
-            .parse::<usize>()
-            .unwrap_or(0),
-    ) {
-        Some(s) => open_file(s).unwrap(),
-        None => open_from_map(map, "That is not a valid entry!"),
-    }
-}
-#[allow(unused_imports)]
-fn pause() {
-    use std::io::{stdin, stdout, Read, Write};
-    let mut stdout = stdout();
-    stdout.write_all(b"Press Enter to continue...").unwrap();
-    stdout.flush().unwrap();
-    stdin().read_exact(&mut [0]).unwrap();
 }
 
 struct ImageDetails {
